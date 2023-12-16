6e9ab0957a8b66eea56aaf9dafeaa6aa1011be13
justinsb <justinsb@google.com>
2023-12-15 22:39:34 -0500

    Bigger model

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ from export import model_export
 # I/O
 out_dir = "out"
 eval_interval = 2000
-log_interval = 1
+log_interval = 100
 eval_iters = 100
 eval_only = False  # if True, script exits right after the first eval
 always_save_checkpoint = False  # if True, always save a checkpoint after each eval
@@ -45,19 +45,19 @@ wandb_log = False  # disabled by default
 wandb_project = "llamac"
 wandb_run_name = "run" + datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 # data
-batch_size = 128  # if gradient_accumulation_steps > 1, this is the micro-batch size
-max_seq_len = 256
+batch_size = 16  # if gradient_accumulation_steps > 1, this is the micro-batch size
+max_seq_len = 1024
 vocab_source = "llama2" # llama2|custom; use Lllama 2 vocab from Meta, or custom trained
 vocab_size = 32000 # the Llama 2 tokenizer has 32K tokens
 # model
-dim = 288
-n_layers = 6
-n_heads = 6
-n_kv_heads = 6
+dim = 768
+n_layers = 12
+n_heads = 12
+n_kv_heads = 12
 multiple_of = 32
 dropout = 0.0
 # adamw optimizer
-gradient_accumulation_steps = 4  # used to simulate larger batch sizes
+gradient_accumulation_steps = 8  # used to simulate larger batch sizes
 learning_rate = 5e-4  # max learning rate
 max_iters = 100000  # total number of training iterations
 weight_decay = 1e-1
